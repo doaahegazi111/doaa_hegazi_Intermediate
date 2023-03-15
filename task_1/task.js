d2a57c4bccda4f9423bbbc8b3f3167be78c11077
Doaa Hegazi <113864564+doaahegazi111@users.noreply.github.com>
2023-03-15 22:16:27 +0200

    Update task.js

--- task_1/task.js
+++ task_1/task.js
@@ -1,11 +1,11 @@
-let arr =[18,150,2,9,58,4];
-function biggest(input){
+let arr = new Array;
+function biggest(x){
     if(arr.length == 1){
-        return null
-    }
-    else{
-        let result= input.sort((x,y) => y-x).slice(0,2);
-        return result;
-    }
+        return null;
+    }else{}
+    let result = x.sort((x,y) => y-x).slice(0,2);
+    return result;
 }
-console.log(biggest(arr));
+// arr=[18];     /*  ============== Output = null */
+arr=[18,150,2,9,58,4];       /* ======== output 150,58 */
+document.write(biggest(arr));
