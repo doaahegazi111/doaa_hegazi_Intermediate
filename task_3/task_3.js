5c43bf42344c44e424e58a155bf7d7b98022dc53
Doaa Hegazi <113864564+doaahegazi111@users.noreply.github.com>
2023-03-15 22:18:23 +0200

    Update task_3.js

--- task_3/task_3.js
+++ task_3/task_3.js
@@ -1,1 +1,18 @@
-
+let arr=[-1,-5,-8]
+let sum=0;
+function addition(num){
+    let counter=0;
+    for(let i=0; i<num.length ; i++){
+        if(num[i]<0){
+            counter++;
+        }
+        else{
+            sum +=num[i];
+        }
+    }
+    if(counter == num.length){
+        return 0;
+    }
+    return sum;
+}
+document.write(addition(arr))
