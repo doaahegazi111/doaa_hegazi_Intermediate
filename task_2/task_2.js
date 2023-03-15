202e258a97d6bd700ecd90dde8c5b7b45d413c61
Doaa Hegazi <113864564+doaahegazi111@users.noreply.github.com>
2023-03-15 22:17:41 +0200

    Update task_2.js

--- task_2/task_2.js
+++ task_2/task_2.js
@@ -1,1 +1,19 @@
-
+let arr= new Array;
+let max= 0 ;
+let index =0;
+function longest(long){
+    if(arr.length == 0){
+        return null;
+    }
+    for(let i =0 ; i < long.length ; i++){
+        let x = long[i].length;
+        if(x > max){
+            max = x;
+            index=i;
+            console.log(arr[index])
+        }
+    }
+    return arr[index];
+}
+arr=["apple","pear","kiwi","banana"]
+document.write(longest(arr))
